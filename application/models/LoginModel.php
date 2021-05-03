749a190e69c62d192c1342d0f95c22c5d86e7094
TIWA RAMDHANI <41172181+Miharada@users.noreply.github.com>
2021-05-03 08:28:48 +0700

    Update LoginModel.php

--- application/models/LoginModel.php
+++ application/models/LoginModel.php
@@ -1,13 +1,23 @@
 <?php
+// Class: LoginModel
+// Class untuk melakukan pengecekan pada database user dan admin saat login
 class LoginModel extends CI_model{
-
+	
+	// Function : check_username
+	// Fungsi untuk melakukan pengecekan username User saat login
     public function check_username(){
+		// Mendapatkan value username dan password dari view
         $username = $this->input->post('username');
         $password = $this->input->post('password');
+		
+		// Membuat Query pengecekan username dan password
         $this->db->where('username',$username);
         $this->db->where('password',$password);
+		
+		// Menyimpan hasil query ke dalam variable result
         $result = $this->db->get('akun') -> result_array();
 
+		// Jika username dan password ditemukan maka return true, else false
         if(count($result)>0){
             return true;
         }
@@ -15,15 +25,22 @@ class LoginModel extends CI_model{
             return false;
         }
     }
-
+	
+	// Function : check_usernameA
+	// Fungsi untuk melakukan pengecekan username Admin saat login
     public function check_usernameA(){
+		// Mendapatkan value username dan password dari view
         $username = $this->input->post('username');
         $password = $this->input->post('password');
-        print($username);
+		
+		// Membuat Query pengecekan username dan password
         $this->db->where('username',$username);
         $this->db->where('password',$password);
+		
+		// Menyimpan hasil query ke dalam variable result
         $result = $this->db->get('admin') -> result_array();
-
+		
+		// Jika username dan password ditemukan maka return true, else false
         if(count($result)>0){
             return true;
         }
