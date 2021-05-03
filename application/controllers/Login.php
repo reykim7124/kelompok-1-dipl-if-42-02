53dc8a9f89af95baf8e804e308ed78fb4e84a6a8
stefanuswisnu <42809599+stefanuswisnu@users.noreply.github.com>
2021-05-03 08:06:36 +0700

    Update Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,7 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+// class untuk Login 
 class Login extends CI_Controller {
+    // Fungsi untuk membangun objek
     public function __construct()
     {
         parent::__construct();
@@ -12,12 +13,12 @@ class Login extends CI_Controller {
 	{
         $this->load->view('Login');
 	}
-
+    // Fungsi untuk melakukan pengecekan form login
     public function login()
     {
         $this->form_validation->set_rules('username','username','required');
         $this->form_validation->set_rules('password','password','required');
-        
+        // Jika form_validation false maka akan menampilkan form, jika true maka akan di cek formnya 
         if($this->form_validation->run() == false){
             $this->load->view('Login');
         } else {
