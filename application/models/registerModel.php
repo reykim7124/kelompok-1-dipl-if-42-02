a57674068cd1e4c62ad600f3c5a60914bfdb20d9
TIWA RAMDHANI <41172181+Miharada@users.noreply.github.com>
2021-05-08 23:30:28 +0700

    Add files via upload

---
+++ application/models/registerModel.php
@@ -0,0 +1,52 @@
+<?php
+// Class: registerModel
+// Class untuk melakukan pengecekan pada database user saat melakukan register
+class registerModel extends CI_model{
+
+    // Function : check_username
+	// Fungsi untuk melakukan pengecekan username User saat login
+    public function check_username(){
+        // Mendapatkan value username dan password dari view
+        $username = $this->input->post('username');
+        $password = $this->input->post('password');
+
+        // Membuat Query pengecekan username dan password
+        $this->db->where('username',$username);
+        $this->db->where('password',$password);
+
+        // Menyimpan hasil query ke dalam variable result
+        $result = $this->db->get('akun') -> result_array();
+
+        // Jika username dan password tidak ditemukan maka return true, else false
+        if(count($result)==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    // Function : check_usernameA
+	// Fungsi untuk melakukan pengecekan username Admin saat register
+    public function check_usernameA(){
+        // Mendapatkan value username dan password dari view
+        $username = $this->input->post('username');
+        $password = $this->input->post('password');
+      
+        // Membuat Query pengecekan username dan password
+        $this->db->where('username',$username);
+        $this->db->where('password',$password);
+
+        // Menyimpan hasil query ke dalam variable result
+        $result = $this->db->get('admin') -> result_array();
+        
+        // Jika username dan password tidak ditemukan maka return true, else false
+        if(count($result)==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+?>
