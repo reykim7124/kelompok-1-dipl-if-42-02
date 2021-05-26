9451f49e07493d4d4a33137db76b8f7fe59d03ce
unknown <80655217+mmiftahh123@users.noreply.github.com>
2021-05-26 13:41:53 +0700

    Update ModelDetailPetisi.php

--- application/models/ModelDetailPetisi.php
+++ application/models/ModelDetailPetisi.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class MPetisi extends CI_Model {
+class ModelDetailPetisi extends CI_Model {
     
     public function getPetisi($table) {
         
