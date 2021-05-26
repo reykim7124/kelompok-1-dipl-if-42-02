f13930f881de9e853f028efea2240b511ebc4451
unknown <80655217+mmiftahh123@users.noreply.github.com>
2021-05-26 13:38:36 +0700

    Controller&Model Detail Petis

---
+++ application/models/ModelDetailPetisi.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MPetisi extends CI_Model {
+    
+    public function getPetisi($table) {
+        
+        return $this->db->get($table);
+    }
+
+    public function detailPetisi($table, $where){
+
+        return $this->db->get_where($table, $where);
+    }
+
+}
+
+?>
